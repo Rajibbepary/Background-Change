4fedb3257d692fa11b02d1e0135fd7dc8d2a5d43
Rajibbepary <rajib635356@gmail.com>
2026-03-18 12:38:08 +0600

    function test

--- index.js
+++ index.js
@@ -6,7 +6,28 @@
 //     return 'Hello world'
 // }
 
-const myFun = (x, y) =>{
-    return x + y
+// const myFun = (x, y) =>{
+//     return x + y
+// }
+//onsole.log(myFun(10 ,20))
+
+// function sayHello (name){
+//     return 'Rajib' + name
+// }
+
+// let fullName = sayHello('raj')
+
+// console.log(fullName)
+
+function myMap(arr, cal) {
+  let result = [];
+  
+  for (let i = 0; i < arr.length; i++) {
+    result.push(cal(arr[i], i));
+  }
+
+  return result;
 }
-onsole.log(myFun(10 ,20))
+
+// test
+console.log(myMap([1,2,3], x => x * 2)); // [2,4,6]
