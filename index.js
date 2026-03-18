39bc8bdc945e223cd2adba79a1c80d4f5f7e6dad
Rajibbepary <rajib635356@gmail.com>
2026-03-18 11:24:13 +0600

    javascript function pratice

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+//function myFun () {return 'Hello world'}
+
+// function myFun (){
+//     return 'Hello world'
+// }
+
+const myFun = (x, y) =>{
+    return x + y
+}
+onsole.log(myFun(10 ,20))
